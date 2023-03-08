d57bba01a0055502f7252edacf86d89779a77bf8
Guilherme Bernardo <gbernardo@magnasistemas.com.br>
2023-03-08 16:57:51 -0300

    ajuste de mensagem

--- src/components/ButtonConfirm/ButtonConfirm.tsx
+++ src/components/ButtonConfirm/ButtonConfirm.tsx
@@ -1,11 +1,18 @@
 import styles from './buttonConfirm.module.scss';
 
 export default function ButtonConfirm() {
+    const message = '*Olá, eu confirmo minha presença.*\n\n*Sugestão de presentes* 🎁 🌈\n1 pacote de fralda M + 1 Mimo\n_ou_\n1 Pacote de fralda G + 1 Mimo\n\n*Informações adicionais:*\n*Data:* 23/04/2023\n*Horário:* das 15:00 até as 20:00\n*Local:* Rua Anum-Dourado, Jardim Dom Jose, 260.\n\n_Nós (papai e mamãe) agradecemos pela presença e todo o carinho 😊_';
+    const formatedMessage = window.encodeURIComponent(message);
+
+    const handleConfirmPresence = ()=> {
+        window.open(`https://api.whatsapp.com/send?phone=+5511965684317&text=${formatedMessage}`);
+    };
+
     return (
-        <a href="https://api.whatsapp.com/send?phone=+5511930339592&text=Olá! Confirmo minha presença e estou de acordo em contribuir com um pacote de fralda e um mimo para o baby 🥰🎁">
-            <button className={styles.btnConfirm}>
+        // <a href={`https://api.whatsapp.com/send?phone=+5511930339592&text=${formatedMessage}`}>
+            <button className={styles.btnConfirm} onClick={handleConfirmPresence}>
                 Confirmar presença
             </button>
-        </a>
+        // </a>
     )
 }
